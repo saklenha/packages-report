f48e7a67c6bf8e0e69bd6a42403ca4b8117af5e6
Sarah Klenha <saklenha@gmail.com>
2019-01-15 17:19:11 -0600

    package report files

---
+++ R/01_write-installed-packages.R
@@ -0,0 +1,27 @@
+## deja vu from earlier!
+
+library(tidyverse)
+library(here)
+
+## create a data frame of your installed packages
+## hint: installed.packages() is the function you need
+
+packages <- installed.packages() %>%
+  as.data.frame()
+
+## optional: select just some of the variables, such as
+##   * Package
+##   * LibPath
+##   * Version
+##   * Priority
+##   * Built
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+
+write_csv(packages,here('data','installed-packages.csv'))
+
+## YES overwrite the file that is there now (or delete it first)
+## that's a old result from me (Jenny)
+## it an example of what yours should look like and where it should go
---
+++ R/03_barchart-packages-built.R
@@ -0,0 +1,23 @@
+## remember to restart R here!
+
+library(tidyverse)
+## make a barchart from the frequency table in data/add-on-packages-freqtable.csv
+
+## read that csv into a data frame
+## hint: readr::read_csv() or read.csv()
+## idea: try using here::here() to create the file path
+
+apt_freqtable <- read_csv(here::here('data','add-on-packages-freqtable.csv'))
+
+## if you use ggplot2, code like this will work:
+gg <- ggplot(apt_freqtable, aes(x = Built, y = n)) +
+  geom_col()
+
+## write this barchart to figs/built-barchart.png
+## if you use ggplot2, ggsave() will help
+## idea: try using here::here() to create the file path
+
+ggsave(here::here('figs','built-barchart.png'),gg)
+
+## YES overwrite the file that is there now
+## that's old output from me (Jenny)
